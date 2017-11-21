0073115252f7b564d76f4c9cab5b24220fc60f6d
SEFDStuff <ankelzspam@gmail.com>
2017-11-21 03:42:29 -0800

    Initial commit

---
+++ PencilsDown/MainActivity.cs
@@ -0,0 +1,345 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System;
+using System.Threading;
+using jsontools;
+using Android.Graphics;
+using Android.Views;
+
+namespace PencilsDown
+{
+    [Activity(Label = "PencilsDown", MainLauncher = true, Theme = "@android:style/Theme.Black.NoTitleBar.Fullscreen")]
+    public class MainActivity : Activity
+    {
+        public int daysCycle = 7;
+        public string channelID = "UCQALLeQPoZdZC4JNUboVEUg";
+
+        int[] dayFrac;
+        int whichVid = 0;
+
+        Random r = new Random();
+
+        // INSERT YOUR PERSONAL API KEY HERE FOR THIS TO WORK AT ALL! If you don't know how to do this, please visit https://developers.google.com/youtube/v3/getting-started & actually read it lol
+        string apiKey = "";
+
+        string json;
+
+        Color[] bg = new Color[]{
+            Color.Rgb(152,251,152),
+            Color.Rgb(80,0,0),
+};
+
+        int[] gfxs = new int[]{
+            // 0
+            Resource.Drawable.target,
+            // 1
+            Resource.Drawable.relax, 
+            // 2
+            Resource.Drawable.working, 
+            // 3
+            Resource.Drawable.wrap, 
+            // 4
+            Resource.Drawable.pencil, 
+            // 5
+            Resource.Drawable.youtube, 
+            // 6
+            Resource.Drawable.late,
+            // 7
+            Resource.Drawable.skull,
+        };
+        int prevHour;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // this demo mode is simply for the video
+            WhichScreen(false);
+        }
+
+        /// <summary>
+        /// Only set this to true if you want to view the raw data, I used this for the video I published
+        /// </summary>
+        /// <param name="demo"></param>
+        void WhichScreen(bool demo)
+        {
+            if (!demo)
+            {
+                // Set our view from the "main" layout resource
+                SetContentView(Resource.Layout.Main);
+
+                // 
+                Button b = FindViewById<Button>(Resource.Id.button1);
+                b.Click += ButClick;
+                b.SetHighlightColor(Color.Aqua);
+
+                // 
+                FindViewById<View>(Resource.Id.inputs).Visibility = ViewStates.Gone;
+
+                FindViewById<EditText>(Resource.Id.editText1).Text = channelID;
+                FindViewById<EditText>(Resource.Id.editText2).Text = "" + daysCycle;
+
+                LoadData();
+
+                //
+                Thread t = new Thread(abc);
+                t.Start();
+            }
+            else
+            {
+                SetContentView(Resource.Layout.demo);
+
+                TextView tV = FindViewById<TextView>(Resource.Id.textView1);
+                tV.SetTextColor(Color.Black);
+                tV.TextSize = 10;
+
+                View v = FindViewById(Resource.Id.top);
+                v.SetBackgroundColor(Color.LightGreen);
+
+                // 
+                string api = Backend.GetFullURL(channelID, apiKey);
+
+                // 
+                json = Backend.DownloadData(api);
+
+                tV.Text = json;
+            }
+        }
+
+        /// <summary>
+        /// This updates the fraction intervals for determining what gfx to display
+        /// </summary>
+        void UpdateDayFract()
+        {
+            // 
+            dayFrac = new int[]
+            {
+                (int)Math.Round(daysCycle*.143f),
+                (int)Math.Round(daysCycle*.286f),
+                (int)Math.Round(daysCycle*.714f),
+                (int)Math.Round(daysCycle*.857f),
+            };
+        }
+
+        // 
+        void ButClick(object sender, EventArgs e)
+        {
+            View v = FindViewById<View>(Resource.Id.inputs);
+            Button b = FindViewById<Button>(Resource.Id.button1);
+            EditText eT1 = FindViewById<EditText>(Resource.Id.editText1);
+            EditText eT2 = FindViewById<EditText>(Resource.Id.editText2);
+
+
+            bool vis = (v.Visibility == ViewStates.Visible);
+
+            v.Visibility = (vis) ? ViewStates.Gone : ViewStates.Visible;
+            b.Text = (vis) ? "Settings" : "Submit";
+
+            if(vis)
+            {
+                if (eT1.Text != channelID)
+                {
+                    channelID = eT1.Text;
+                    LoadData();
+                }
+
+                if (eT2.Text != ""+daysCycle)
+                {
+                    daysCycle = int.Parse(eT2.Text);
+                    UpdateUI();
+                }
+            }
+        }
+
+        // 
+
+        // 
+        void abc()
+        {
+            // 
+            while (true)
+            {
+                RunOnUiThread(Update);
+                Thread.Sleep((1000*60));
+            }
+        }
+
+        // 
+        void Update()
+        {
+            // 
+            if (DateTime.Now.Hour != prevHour)
+            {
+                LoadData();
+            }
+        }
+
+        // 
+        void LoadData()
+        {
+            // 
+            ImageView task = FindViewById<ImageView>(Resource.Id.imageView1);
+            ImageView thumb = FindViewById<ImageView>(Resource.Id.imageView2);
+            TextView tV = FindViewById<TextView>(Resource.Id.textView1);
+            View v = FindViewById(Resource.Id.top);
+
+            // 
+            tV.SetBackgroundColor(Color.White);
+
+            // 
+            string api = Backend.GetFullURL(channelID, apiKey);
+
+            // 
+            json = Backend.DownloadData(api);
+
+            // 
+            if (!json.Contains("Error"))
+            {
+                // 
+                Videos ch = JsonConvert.DeserializeObject<Videos>(json);
+
+                // 
+                int DaysAgo = Backend.DateSubtraction(ch.items[whichVid].snippet.publishedAt);
+
+                // 
+                v.SetBackgroundColor(FindBGCol(bg[0], bg[1], DaysAgo));
+
+                // 
+                var imageBitmap = Backend.GetImageBitmapFromUrl(ch.items[whichVid].snippet.thumbnails.standard.url);
+                thumb.SetImageBitmap(imageBitmap);
+
+                // 
+                tV.SetTextColor(Color.Black);
+                tV.Text = "You last uploaded " + ch.items[whichVid].snippet.title + " " + DaysAgo + " days ago.";
+                tV.Gravity = GravityFlags.Center;
+
+                // 
+                task.SetImageResource(DrawGFX(DaysAgo));
+
+                // 
+                prevHour = DateTime.Now.Hour;
+            }
+            else if(json.Contains("Error"))
+            {
+                task.SetImageResource(0);
+                thumb.SetImageResource(0);
+                tV.Text = json;
+            }
+            else
+            {
+                task.SetImageResource(0);
+                thumb.SetImageResource(0);
+                tV.Text = "CHANNEL DOESN'T EXIST! PLEASE TRY AGAIN!";
+            }
+        }
+
+        // 
+        void UpdateUI()
+        {
+            // 
+            ImageView task = FindViewById<ImageView>(Resource.Id.imageView1);
+            ImageView thumb = FindViewById<ImageView>(Resource.Id.imageView2);
+            TextView tV = FindViewById<TextView>(Resource.Id.textView1);
+            View v = FindViewById(Resource.Id.top);
+
+            // 
+            Videos ch = JsonConvert.DeserializeObject<Videos>(json);
+
+            // 
+            int DaysAgo = Backend.DateSubtraction(ch.items[whichVid].snippet.publishedAt);
+
+            // 
+            v.SetBackgroundColor(FindBGCol(bg[0], bg[1], DaysAgo));
+
+            // 
+            var imageBitmap = Backend.GetImageBitmapFromUrl(ch.items[whichVid].snippet.thumbnails.standard.url);
+            thumb.SetImageBitmap(imageBitmap);
+
+            // 
+            tV.SetTextColor(Color.Black);
+            tV.Text = "You last uploaded " + ch.items[whichVid].snippet.title + " " + DaysAgo + " days ago.";
+            tV.Gravity = GravityFlags.Center;
+
+            // 
+            task.SetImageResource(DrawGFX(DaysAgo));
+        }
+
+        // 
+        Color FindBGCol(Color color1, Color color2, int dA)
+        {
+            int[] loss = new int[3];
+            loss[0] = color1.R - color2.R;
+            loss[1] = color1.G - color2.G;
+            loss[2] = color1.B - color2.B;
+
+            int[] nRBG = new int[3];
+
+            float[] transpose = new float[2];
+            transpose[0] = dA;
+            transpose[1] = daysCycle;
+
+            nRBG[0] = (color1.R - (int)(loss[0] * Clamp((float)dA / (float)daysCycle)));
+            nRBG[1] = (color1.G - (int)(loss[1] * Clamp((float)dA / (float)daysCycle)));
+            nRBG[2] = (color1.B - (int)(loss[2] * Clamp((float)dA/ (float)daysCycle)));
+
+            return new Color(nRBG[0], nRBG[1], nRBG[2]);
+        }
+
+        /// <summary>
+        /// This is used to clamp the r g b values of the background from 0-1f
+        /// </summary>
+        /// <param name="v"></param>
+        /// <returns></returns>
+        float Clamp(float v)
+        {
+            v = (v > 1) ? 1 : (v < 0) ? 0 : v;
+            return v;
+        }
+
+        //
+        int DrawGFX(int DaysAgo)
+        {
+            UpdateDayFract();
+
+            int ret = 0;
+
+            // 
+            if (DaysAgo >= 0 && DaysAgo < dayFrac[0])
+            {
+                ret = gfxs[0];
+            }
+            else if (DaysAgo >= dayFrac[0] && DaysAgo < dayFrac[1])
+            {
+                ret = gfxs[1];
+            }
+            else if (DaysAgo >= dayFrac[1] && DaysAgo < dayFrac[2])
+            {
+                ret = gfxs[2];
+            }
+            else if (DaysAgo >= dayFrac[2] && DaysAgo < dayFrac[3])
+            {
+                ret = gfxs[3];
+            }
+            else if (DaysAgo >= dayFrac[3] && DaysAgo < daysCycle)
+            {
+                ret = gfxs[4];
+            }
+            else if (DaysAgo == daysCycle)
+            {
+                ret = gfxs[5];
+            }
+            else if (DaysAgo > daysCycle && DaysAgo < daysCycle*2)
+            {
+                ret = gfxs[6];
+            }
+            else if (DaysAgo >= daysCycle*2)
+            {
+                ret = gfxs[7];
+            }
+            return ret;
+        }
+    }
+}
---
+++ PencilsDown/backend.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using Android.Graphics;
+
+namespace jsontools
+{
+    public class Backend
+    {
+        /// <summary>
+        /// this generates a full api url using channel id & api key
+        /// </summary>
+        /// <param name="channelID">The ID for your channel, the one formated with random characters seems to be the only one that works</param>
+        /// <param name="apiKey">The API key that youre going to use to pull request from YouTube's servers</param>
+        /// <returns></returns>
+        public static string GetFullURL(string channelID, string apiKey)
+        {
+            return String.Format("https://www.googleapis.com/youtube/v3/activities?part=snippet,contentDetails&channelId={0}&key={1}", channelID, apiKey);
+        }
+
+        /// <summary>
+        /// this function downloads the data from the web & gives you that data as a string
+        /// </summary>
+        /// <param name="api">use the GetFullURL function</param>
+        /// <returns></returns>
+        public static string DownloadData(string api)
+        {
+            string ret = "";
+
+            try
+            {
+                WebClient wC = new WebClient();
+                ret = wC.DownloadString(api);
+            }
+            catch(Exception e)
+            {
+                ret = e.Message;
+            }
+
+            return ret;
+        }
+
+        void wc_DownloadStringCompleted(object sender, DownloadStringCompletedEventArgs e)
+        {
+            string text = e.Result;
+
+        }
+        /// <summary>
+        /// This function does the complicated task of date subtraction for you, it compares the past date to today
+        /// </summary>
+        /// <param name="pastDate">Enter the past date</param>
+        /// <returns></returns>
+        public static int DateSubtraction(DateTime pastDate)
+        {
+            int ret = DateTime.Today.Subtract(pastDate).Days+1;
+
+            return ret;
+        }
+
+        // 
+        public static Bitmap GetImageBitmapFromUrl(string url)
+        {
+            Bitmap imageBitmap = null;
+
+            using (var webClient = new WebClient())
+            {
+                var imageBytes = webClient.DownloadData(url);
+                if (imageBytes != null && imageBytes.Length > 0)
+                {
+                    imageBitmap = BitmapFactory.DecodeByteArray(imageBytes, 0, imageBytes.Length);
+                }
+            }
+
+            return imageBitmap;
+        }
+
+    }
+
+    public class Videos
+    {
+        public List<Item> items { get; set; }
+
+        public class Item
+        {
+            public Snip snippet { get; set; }
+
+            public class Snip
+            {
+                public DateTime publishedAt { get; set; }
+                public string title;
+                public Thumbs thumbnails { get; set; }
+
+                public class Thumbs
+                {
+                    public High high { get; set; }
+                    public Standard standard { get; set; }
+
+                    public class High
+                    {
+                        public string url { get; set; }
+                    }
+
+                    public class Standard
+                    {
+                        public string url { get; set; }
+                    }
+                }
+            }
+        }
+    }
+
+}
